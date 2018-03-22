8e8c89d97d6c356c1e5ff8c7246507dd8011c08f
Alejandro Ferrero <alejandroferrero@outlook.com>
2018-03-21 20:57:47 -0400

    Modified request package

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
     "extends": "airbnb",
     "rules": {
       "no-console": 0,
-      "import/no-dynamic-require": 0
+      "import/no-dynamic-require": 0,
+      "func-names": ["error", "never"]
     }
 };
--- index.js
+++ index.js
@@ -4,11 +4,24 @@ const {
   parser,
 } = require('./lib/utils.js');
 
+const prepareToUpdate = (function () {
+  let counter = 0;
+  let gpsPackage = [];
+  return (gpsInfo) => {
+    gpsPackage.push(gpsInfo);
+    counter += 1;
+    if (counter >= 10) {
+      update(gpsPackage);
+      counter = 0;
+      gpsPackage = [];
+    }
+  };
+}());
+
 gps.on('data', (data) => {
   const parts = data.split(',');
   if (parts[0] === '$GPRMC' && parts[2] !== 'V') {
-    const gpsInfo = parser(parts);
-    update(gpsInfo);
+    prepareToUpdate(parser(parts));
   }
 });
 
